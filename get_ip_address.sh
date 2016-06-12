546f7a6819ce6d893492121101fcdc37f8d4ffaa
minisha <mmurugan@palo-it.com>
2016-06-12 21:33:55 +0800

    reference added

--- get_ip_address.sh
+++ get_ip_address.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#http://stackoverflow.com/questions/5947742/how-to-change-the-output-color-of-echo-in-linux
+
 IP=$(ifconfig | awk 'NR==12' | awk '{print $2}')
 tput setaf 2
 tput bold 
